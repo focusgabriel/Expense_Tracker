47a6ea40476b4f640d967f94bc78751a5264a897
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-09 12:59:02 +0100

    added the monthlyIncome function endpoint

--- Server/src/controllers/transaction.controllers.ts
+++ Server/src/controllers/transaction.controllers.ts
@@ -40,3 +40,79 @@ export async function getTransaction(req:Request, res:Response){
     console.log("error:",err);
   }
 };
+
+export async function getMonthlyIncome(req:Request, res:Response) {
+// getting the first day of the previous month and the first day of the next month
+const now = new Date()
+const startOfPrevMonth = new Date(
+  now.getFullYear(),
+  now.getMonth() - 1
+)
+const endOfLastMonth = new Date(
+  now.getFullYear(),
+  now.getMonth(),
+)
+const startOfNextMonth = new Date(
+  now.getFullYear(),
+  now.getMonth() + 1,
+)
+// console.log("new Month:", startOfNextMonth)
+// aggregating the monthly expense based on gte and lt which would be based on the highest date(last day of the month) and the beginning of the new month.
+try {
+  const getMonthlyExpense = await ExpenseModel.aggregate([{
+    $match: {
+      type: "expense",
+      date: {
+        $gte: endOfLastMonth,
+        $lt: startOfNextMonth
+      }
+    }
+  }])
+  console.log()
+  const getMonthlyIncome = await ExpenseModel.aggregate([{
+    $match: {
+      type: "income",
+      date: {
+        $gte: endOfLastMonth,
+        $lt: startOfNextMonth
+      }
+    }
+  }])
+
+  const getPrevMonthlyIncome = await ExpenseModel.aggregate([{
+    $match: {
+      type: "income",
+      date: {
+        $gte: startOfPrevMonth,
+        $lt: endOfLastMonth
+      }
+    }
+  }])
+
+  const getPrevMonthlyExpense = await ExpenseModel.aggregate([{
+    $match: {
+      type: "expense",
+      date: {
+        $gte: startOfPrevMonth,
+        $lt: endOfLastMonth
+      }
+    }
+  }])
+
+  // calculating for the previous month
+  const lastMonthIncome = getPrevMonthlyIncome.map((item, index) => item.amount).reduce((value, sum) => value + sum, 0)
+  const lastMonthExpense = getPrevMonthlyExpense.map((item, index) => item.amount).reduce((value, sum) => value + sum, 0)
+  const lastMonthNetBalance = lastMonthIncome - lastMonthExpense;
+
+  // calculating for the current month
+  const get_expense = getMonthlyExpense.map((item, index) => item.amount).reduce((value, sum) => value + sum, 0)
+  const get_income = getMonthlyIncome.map((item, index) => item.amount).reduce((value, sum) => value + sum, 0)
+  const netbalance = get_income - get_expense;
+
+  res.status(200).json({get_expense, get_income, netbalance, lastMonthNetBalance, getMonthlyExpense, endOfLastMonth});
+  
+} catch (error) {
+  res.status(500).json({message: "Server Error"})
+  console.log(error)
+}
+};
