0bce3c23e9f2a1edd890d4940b21befff3c43b3a
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-09 12:59:48 +0100

    added the getMonthlyIncome endpoint router

--- Server/src/routes/transaction.routes.ts
+++ Server/src/routes/transaction.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from "express"
-import { addTransactionController, getTransaction, totalTransactionController } from "../controllers/transaction.controllers.js";
+import { addTransactionController, getMonthlyIncome, getTransaction, totalTransactionController } from "../controllers/transaction.controllers.js";
 const router = Router();
 
 router.post("/addTransaction", addTransactionController)
 router.get("/totalTransaction", totalTransactionController)
 router.get("/getTransaction", getTransaction)
+router.get("/getMonthlyIncome", getMonthlyIncome)
 
 export default router
