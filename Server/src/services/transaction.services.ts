bdb12c80f2edc627df21206c27c537d14681cf2c
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-09 12:47:09 +0100

    created a teansaction file in service folder

---
+++ Server/src/services/transaction.services.ts
@@ -0,0 +1,18 @@
+import { ExpenseModel } from "../db/index.js";
+
+
+
+export async function addTransaction(type:"income" | "expense", amount:number, category:string, description:string, date:Date, created_date:Date){
+    const newTransaction = new ExpenseModel({
+    type,
+    amount, 
+    category,
+    description, 
+    date, 
+    created_date,
+  })
+
+  await newTransaction.save();
+  // console.log(newTransaction);
+  return newTransaction;
+  }
