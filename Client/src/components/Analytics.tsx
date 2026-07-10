461aea04178847df9658c2efdc9ed39e0dc29e09
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-10 22:39:04 +0100

    created a new component to view all transaction

---
+++ Client/src/components/Analytics.tsx
@@ -0,0 +1,55 @@
+/** @format */
+
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import { Link } from "react-router-dom";
+// import type { Transaction } from "../constants"
+
+type Transaction = {
+  _id: string;
+  type: "income" | "expense";
+  amount: number;
+  category: string;
+  description: string;
+  date: Date;
+  created_date: string;
+};
+
+const Analytics = () => {
+  const [trans, setTrans] = useState<Transaction[]>([]);
+
+  // getting the whole data from the database
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/v1/getTransaction`)
+      .then(res => res.json())
+      .then(data => setTrans(data));
+  }, []);
+
+  return (
+    <div className="w-full rounded-lg border border-slate-200 bg-white shadow-sm">
+      <div className="flex justify-between items-center border-b border-slate-100 px-3 py-2 text-xs font-semibold uppercase tracking-wide text-black">
+        <h2>
+          Recent Transactions
+        </h2>
+
+        <Link to="/analytics" className="lowercase text-sm text-blue-600">View All</Link>
+      </div>
+      {trans
+        .map(item => (
+          <Card
+            key={item._id}
+            id={item._id}
+            type={item.type}
+            amount={item.amount}
+            category={item.category}
+            description={item.description}
+            date={item.date}
+            created_date={item.created_date}
+          />
+        ))}
+      {/* splice to render only the first 5 from the database making it the top five recent transaction */}
+    </div>
+  );
+};
+
+export default Analytics;
