59172d066355be2bbbdb73176e9d052dd2cef44e
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-10 21:48:05 +0100

    i added an editTranaction function for editing(update) the data

--- Server/src/services/transaction.services.ts
+++ Server/src/services/transaction.services.ts
@@ -14,3 +14,16 @@ export async function addTransaction(type:"income" | "expense", amount:number, c
   // console.log(newTransaction);
   return newTransaction;
   }
+
+export async function editTransaction(type:"income" | "expense", amount:number, category:string, description:string, date:Date, created_date:Date){
+    const updateTransaction = await ExpenseModel.findByIdAndUpdate(
+      {_id: "6a514e64545fbeea01883574"},
+      {$set: {type, amount, category, description, date, created_date}}
+    )
+    
+
+
+  // await newTransaction.save();
+  // console.log(newTransaction);
+  return updateTransaction;
+  }
