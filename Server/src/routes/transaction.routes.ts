7e0c2e0cc7dfc709cf1f65deb38c43f6f7cd0946
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-10 01:43:14 +0100

    created the put and get routes

--- Server/src/routes/transaction.routes.ts
+++ Server/src/routes/transaction.routes.ts
@@ -1,10 +1,12 @@
 import {Router} from "express"
-import { addTransactionController, getMonthlyIncome, getTransaction, totalTransactionController } from "../controllers/transaction.controllers.js";
+import { addTransactionController, editTransactionControler, getMonthlyIncomeController, getTransactionByIdController, getTransactionController, totalTransactionController } from "../controllers/transaction.controllers.js";
 const router = Router();
 
 router.post("/addTransaction", addTransactionController)
 router.get("/totalTransaction", totalTransactionController)
-router.get("/getTransaction", getTransaction)
-router.get("/getMonthlyIncome", getMonthlyIncome)
+router.get("/getTransaction", getTransactionController)
+router.get("/getMonthlyIncome", getMonthlyIncomeController)
+router.patch("/", editTransactionControler)
+router.get("/getTransactionById/:id", getTransactionByIdController)
 
 export default router
