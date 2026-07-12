f4e92e5210668e1f86013f49ca0d337146c1047b
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-12 23:28:43 +0100

    implemented a type for the user validation

---
+++ Server/src/types/users.types.ts
@@ -0,0 +1,11 @@
+export type RegisterRequestBody = {
+  name:string,
+  email:string,
+  password:string,
+  confirm_password:string
+}
+
+export type LoginRequestBody = {
+  email:string,
+  password:string
+}
