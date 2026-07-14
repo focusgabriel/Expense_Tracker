5ab6f40a18d05e07bff16d063b0559c5cb53537a
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-14 13:29:52 +0100

    implemented the localstorage and added the authorization bearer

--- Client/src/components/AddTask.tsx
+++ Client/src/components/AddTask.tsx
@@ -23,12 +23,14 @@ const AddTask = () => {
     };
     console.log(newTransaction)
     try {
+      const token = localStorage.getItem("token")
       const response = await fetch(
         "http://localhost:3000/api/v1/addTransaction",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
           },
           body: JSON.stringify(newTransaction),
         },
