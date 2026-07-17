21a20d823ad9b01a84959951630a325b4970e89e
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-17 18:15:55 +0100

    implemented interceptor for axios for request and response

--- Client/src/api/fetch.ts
+++ Client/src/api/fetch.ts
@@ -1,17 +1,76 @@
-// import axios from "axios";
+import axios,{
+  type AxiosError,
+  type AxiosRequestConfig,
+  type InternalAxiosRequestConfig,
+} from "axios";
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  _retry?: boolean;
+}
+const refreshClient = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+});
 
-// const api = axios.create({
-//   baseURL: "http://localhost:3000/api/v1",
-// });
+refreshClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem("accessToken");
 
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
 
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
+  return config;
+});
 
-//   return config;
-// });
+// export default refreshClient;
 
-// export default api;
+
+  refreshClient.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  
+  async(error: AxiosError) => {
+    if(error.response?.status === 401){
+      const originalRequest = error.config as CustomAxiosRequestConfig;
+
+      if(error.response?.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true
+
+        const refreshToken = localStorage.getItem("refreshToken");
+        if(!refreshToken){
+          return Promise.reject(error);
+        }
+       try { 
+        const response = await refreshClient.post("/refresh", {
+          refreshToken,
+        });
+
+        const {accessToken, refreshToken:newRefreshToken} = response.data;
+        localStorage.setItem(
+          "accessToken",
+          accessToken
+        )
+
+        localStorage.setItem(
+          "refreshToken",
+          newRefreshToken
+        )
+
+        originalRequest.headers = originalRequest.headers ?? {}
+        originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`
+        return refreshClient(originalRequest);
+      } catch {
+      
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
+      }
+
+      return Promise.reject(error);
+    }
+
+  }
+)
+
+export default refreshClient;
