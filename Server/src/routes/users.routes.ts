8cbdd8853f1a68642c40a7ff7403d485a2680157
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-17 18:20:45 +0100

    impolemented the routes to the refreshToken

--- Server/src/routes/users.routes.ts
+++ Server/src/routes/users.routes.ts
@@ -1,10 +1,10 @@
 import {Router} from "express";
-import { loginController, RegisterController } from "../controllers/user.controllers.js";
+import { loginController, refreshTokenController, RegisterController } from "../controllers/user.controllers.js";
 
 const router = Router();
 
 router.post("/auth/register", RegisterController);
 router.post("/auth/login", loginController);
-// router.post("/refresh", refreshTokenController);
+router.post("/refresh", refreshTokenController);
 
 export default router;
