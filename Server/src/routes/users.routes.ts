9476253ad870baf9f90592573c6f3f7bceee7253
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-19 03:21:40 +0100

    added logout routes

--- Server/src/routes/users.routes.ts
+++ Server/src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import {Router} from "express";
 import { loginController, refreshTokenController, RegisterController } from "../controllers/user.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middlewares.js";
+import { logoutController } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post("/auth/register", RegisterController);
 router.post("/auth/login", loginController);
 router.post("/refresh", refreshTokenController);
+router.post("/auth/logout", authMiddleware, logoutController);
 
 export default router;
