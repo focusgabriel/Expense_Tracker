b9d815f2f339fed423b5488c770a6baab1b0bac0
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-22 23:43:32 +0100

    removed from my codebase

--- Client/src/pages/DeletePage.tsx
+++ Client/src/pages/DeletePage.tsx
@@ -1,30 +1,35 @@
-// import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom"
-import refreshClient from "../api/fetch";
+// // import { useEffect, useState } from "react";
+// import toast from "react-hot-toast";
+// import { Link, useParams } from "react-router-dom"
+// import refreshClient from "../api/fetch";
 
-const DeletePage = () => {
-  // const [transact, setTransact] = useState()
-  const { id } = useParams();
+// const DeletePage = () => {
+//   // const [transact, setTransact] = useState()
+//   const { id } = useParams();
 
-  const handleClick = () => {
+//   const handleClick = () => {
 
-    // fetch(`/deleteTransaction/${id}`)
+//     // fetch(`/deleteTransaction/${id}`)
 
-    refreshClient.delete(`/deleteTransaction/${id}`)
-  }
+//     refreshClient.delete(`/deleteTransaction/${id}`)
+//     toast.success("Item Deleted Successfully!", {
+//       position: "top-right",
+//       duration: 5000,
+//     });
+//   }
 
  
-  return (
-    <div className="h-screen">
-      <div className="w-full md:w-[50%] md:mx-auto min-h-[50vh] sm:p-10 p-4 flex flex-col items-center justify-center">
-        <h2 className="md:text-2xl text-xl text-center">Are you sure, you want to delete this item</h2>
-        <div className="mt-6 flex gap-4">
-          <Link to="/analytics"><button onClick={handleClick} className="bg-red-600 text-white font-medium p-2 rounded-lg w-24 sm:w-28 cursor-pointer">Proceed</button></Link>
-          <Link to="/analytics"><button className="bg-indigo-500 text-white font-medium p-2 w-24 sm:w-28 rounded-lg cursor-pointer">Cancel</button></Link>
-        </div>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="h-screen">
+//       <div className="w-full md:w-[50%] md:mx-auto min-h-[50vh] sm:p-10 p-4 flex flex-col items-center justify-center">
+//         <h2 className="md:text-2xl text-xl text-center">Are you sure, you want to delete this item</h2>
+//         <div className="mt-6 flex gap-4">
+//           <Link to="/analytics"><button onClick={handleClick} className="bg-red-600 text-white font-medium p-2 rounded-lg w-24 sm:w-28 cursor-pointer">Proceed</button></Link>
+//           <Link to="/analytics"><button className="bg-indigo-500 text-white font-medium p-2 w-24 sm:w-28 rounded-lg cursor-pointer">Cancel</button></Link>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default DeletePage
+// export default DeletePage
