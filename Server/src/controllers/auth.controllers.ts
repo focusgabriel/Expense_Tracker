9b491d0d8a70b77772fc4cb105e52a3c87322cf8
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-25 15:59:52 +0100

    implemented the resetPassword and forgotpassword

--- Server/src/controllers/auth.controllers.ts
+++ Server/src/controllers/auth.controllers.ts
@@ -1,6 +1,7 @@
 import { Response, Request, NextFunction } from "express";
 import { authModel } from "../model/index.js";
 import crypto from "crypto";
+import bcrypt from "bcrypt"
 import { AppError } from "../utils/AppError.js";
 
 export async function logoutController(req:Request, res:Response) {
@@ -82,3 +83,90 @@ export const verifyEmailController = async (
     console.log("failed to verify email...")
   }
 };
+
+export const ForgotPasswordController = async(
+  req:Request,
+  res:Response,
+  next:NextFunction
+) => {
+  const { email } = req.body;
+  try {
+
+    const user = await authModel.findOne({ email });
+
+    if (!user) {
+      return res.status(200).json({
+        status: true,
+        message: "If an account with that email exists, a reset link has been sent."
+      });
+    }
+
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    user.passwordResetToken = hashedToken;
+
+    user.passwordResetExpires = new Date(
+      Date.now() + 1000 * 60 * 15
+    );
+
+    await user.save();
+
+    return res.status(200).json({
+      message: "If an account with that email exists, a reset link has been sent."
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const ResetPasswordController = async(
+  req:Request,
+  res:Response,
+  next:NextFunction
+) => {
+  const { token } = req.params
+  const { password } = req.body;
+  try {
+
+    if(typeof token !== "string" || token?.length === 0){
+      return res.status(400).json({message: "token is invalid."})
+    }
+    
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+
+    const user = await authModel.findOne({
+      passwordHashed: hashedToken,
+      passwordResetExpires: {
+        $gt: new Date()
+      }
+    })
+
+    if (!user) {
+      throw new AppError(
+        "Reset token is invalid or has expired.",
+        400
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+
+    user.passwordResetToken = undefined;
+
+    user.passwordResetExpires = undefined;
+
+    user.refreshToken = null;
+
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Password reset successful.",
+    });
+
+  } catch (error) {
+    
+  }
+}
