34f7ddb66d00faa0aded8e816420c2bb74e92dd5
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-25 21:44:53 +0100

    implemented a rate limiter from expres-rate-limiter library

---
+++ Server/src/validation/limiter.ts
@@ -0,0 +1,10 @@
+import rateLimit from "express-rate-limit";
+
+export const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: {
+    success: false,
+    message: "Too many requests. Please try again later.",
+  },
+});
