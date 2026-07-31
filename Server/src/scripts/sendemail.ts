aff701325e065883bc55e8b080c6e777f37614e2
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-31 16:58:28 +0100

    fixed the build issues after making changes by deleting sendemail adn email.services for redeployment

--- Server/src/scripts/sendemail.ts
+++
@@ -1,5 +0,0 @@
-import { sendEmail } from "../services/email.services.js"
-
-const send = () => {
-  sendEmail({to:"charlesuchendu750@gmail.com", subject:"Hello form focus", html:"<h1>Noreply</h1>"});
-}
